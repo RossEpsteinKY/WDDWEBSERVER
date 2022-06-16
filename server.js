1baa63e0704e50f80ba823317dcd9da550649760
RossEpsteinKY <31447738+RossEpsteinKY@users.noreply.github.com>
2022-06-16 09:35:16 -0400

    added routes to server.js file

--- server.js
+++ server.js
@@ -2,15 +2,27 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
+
 app.use(express.static(__dirname));
 
 app.get('/', (req,res) =>{
     console.log(__dirname);
+    console.log("home was clicked");
     res.sendFile(__dirname + '/home.html');
 });
 
-http.createServer(app).listen(8085, ()=>
-    {
-        console.log("Server running on 8085")
-    }
-);
+
+app.get('/about', (req,res) =>{
+    console.log("about was clicked");
+    res.sendFile(__dirname + '/about.html');
+});
+
+app.get('/portfolio', (req,res) =>{
+    console.log("portfolio was clicked");
+    res.sendFile(__dirname + '/portfolio.html');
+});
+
+
+http.createServer(app).listen(8085, ()=>{
+    console.log('SERVER STARTED ON PORT 8085');
+});
